aade0a3cf6db22b312aba7145c65396badb71b6f
David Bayendor <dbayendor@gmail.com>
2014-01-10 10:35:37 -0700

    First attempt at leap year problem

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -8,6 +8,14 @@ class Year
   end
 
   def leap?
-    year % 4 == 0 && year % 400 == 0
+    if year % 400 == 0
+      true
+    elsif year % 100 == 0 && year % 4 == 0
+      false
+    elsif year % 4 == 0
+      true
+    else
+      false
+    end
   end
 end
