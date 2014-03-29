a915c40faf78933003cb83b84244915c096698a8
David Bayendor <dbayendor@gmail.com>
2014-03-29 10:00:09 -0600

    Abstract modulo calculations to verbose methods
    * Nitpick suggested intention revealing code

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -8,14 +8,32 @@ class Year
   end
 
   def leap?
-    if year % 400 == 0
+    if divisible_by_400?(year)
       true
-    elsif year % 100 == 0 && year % 4 == 0
+    elsif divisible_by_100?(year)
       false
-    elsif year % 4 == 0
+    elsif ordinary_leap_year?(year)
       true
     else
       false
+    end
+  end
+
+  def divisible_by_400?(year)
+    if year % 400 == 0
+      true
+    end
+  end
+
+  def divisible_by_100?(year)
+    if year % 100 == 0 && year % 4 == 0
+      true
+    end
+  end
+
+  def ordinary_leap_year?(year)
+    if year % 4 == 0
+      true
     end
   end
 end
