1b48dbb5cb3295e83c5491d1831aa7d8bd9d6737
David Bayendor <dbayendor@gmail.com>
2014-03-29 11:03:33 -0600

    Grains of rice on a chessboard problem

---
+++ ruby/grains/grains.rb
@@ -0,0 +1,15 @@
+# Exercism.io: Grains of rice on a chessboard problem
+class Grains
+  attr_accessor :square
+
+  def initialize
+  end
+
+  def square(square)
+    2**(square - 1)
+  end
+
+  def total
+    2**64 - 1
+  end
+end
