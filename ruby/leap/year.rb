ab5d4ce59eca5519ef8574b55d9e5a7e5c64b62d
David Bayendor <dbayendor@gmail.com>
2014-03-30 08:15:52 -0600

    Refactor leap_year to use booleans
    * Simplify code, remove if's.

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,5 +1,4 @@
 # year: exercism leap year tester
-
 class Year
   attr_accessor :year
 
@@ -8,32 +7,19 @@ class Year
   end
 
   def leap?
-    if divisible_by_400?(year)
-      true
-    elsif divisible_by_100?(year)
-      false
-    elsif ordinary_leap_year?(year)
-      true
-    else
-      false
-    end
+    ordinary_leap_year?(year) && (!ordinary_century?(year) ||
+    exceptional_century?(year))
   end
 
-  def divisible_by_400?(year)
-    if year % 400 == 0
-      true
-    end
+  def exceptional_century?(year)
+    year % 400 == 0
   end
 
-  def divisible_by_100?(year)
-    if year % 100 == 0 && year % 4 == 0
-      true
-    end
+  def ordinary_century?(year)
+    year % 100 == 0
   end
 
   def ordinary_leap_year?(year)
-    if year % 4 == 0
-      true
-    end
+    year % 4 == 0
   end
 end
