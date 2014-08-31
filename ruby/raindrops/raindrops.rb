2f749553154a314bd01e018455578a41b794891a
David Bayendor <dbayendor@gmail.com>
2014-08-31 11:20:34 -0600

    Reduce complexity
    * Move raindrop language to #find_rain
    
    * Convert to @instance variables

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,23 +2,26 @@ class Raindrops
   require 'prime'
 
   def self.convert(number)
-    factors = number.prime_division.flatten!
-    raindrops = ''
+    @raindrops = ''
+    @factors = number.prime_division.flatten!
 
-    if factors.nil?
+    if @factors.nil?
       1
     else
-      factors.each do |n|
-        if n == 3
-          raindrops += 'Pling'
-        elsif n == 5
-          raindrops += 'Plang'
-        elsif n == 7
-          raindrops += 'Plong'
-        end
+      find_rain(@factors)
+    end
+    @raindrops.empty? ? number.to_s : @raindrops
+  end
+
+  def self.find_rain(factors)
+    factors.each do |n|
+      if n == 3
+        @raindrops += 'Pling'
+      elsif n == 5
+        @raindrops += 'Plang'
+      elsif n == 7
+        @raindrops += 'Plong'
       end
     end
-
-    raindrops.empty? ? number.to_s : raindrops
   end
 end
