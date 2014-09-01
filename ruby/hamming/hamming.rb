b141592239c21893af0e23bf8ca01011ee8ee801
David Bayendor <dbayendor@gmail.com>
2014-08-31 20:11:51 -0600

    Clean up difference calculation

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,11 +1,11 @@
 class Hamming
   def self.compute(sequence_1, sequence_2)
-    distance = 0
     difference = (sequence_1.length - sequence_2.length).abs.to_i
+    distance = 0 - difference
 
     0.upto(sequence_1.length) do |base|
       sequence_1[base] == sequence_2[base] || distance += 1
     end
-    distance - difference
+    distance
   end
 end
