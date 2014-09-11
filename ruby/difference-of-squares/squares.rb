c59ac85ec5aa1d2d19650dd05b436b32de08808e
David Bayendor <dbayendor@gmail.com>
2014-09-10 19:26:52 -0600

    Ruby Difference of Squares first pass
    * All tests passing

--- ruby/difference-of-squares/squares.rb
+++ ruby/difference-of-squares/squares.rb
@@ -1,9 +1,17 @@
 class Squares
-  def initialize(args)
-
+  def initialize(n)
+    @n = n
   end
 
   def square_of_sums
+    (1..@n).inject(:+)**2
+  end
 
+  def sum_of_squares
+    (1..@n).map { |n| n**2 }.reduce(:+)
+  end
+
+  def difference
+    square_of_sums - sum_of_squares
   end
 end
