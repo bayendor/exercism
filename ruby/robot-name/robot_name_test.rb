48ac60a732c0b5f788339ce44061ba932fc28423
David Bayendor <dbayendor@gmail.com>
2014-10-05 14:00:59 -0600

    Ruby robot name

--- ruby/robot-name/robot_name_test.rb
+++ ruby/robot-name/robot_name_test.rb
@@ -1,25 +1,22 @@
 require 'minitest/autorun'
 require_relative 'robot'
 
-class RobotTest < MiniTest::Unit::TestCase
+class RobotTest < MiniTest::Test
   def test_has_name
     assert_match /\w{2}\d{3}/, Robot.new.name
   end
 
   def test_name_sticks
-    skip
     robot = Robot.new
     robot.name
     assert_equal robot.name, robot.name
   end
 
   def test_different_robots_have_different_names
-    skip
     assert Robot.new.name != Robot.new.name
   end
 
   def test_reset_name
-    skip
     robot = Robot.new
     name = robot.name
     robot.reset
